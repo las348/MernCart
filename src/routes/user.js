13132d8235f443d82d70d1b53e8506fb56ac5b6e
Laureni Wilkinson <las348@nyu.edu>
2020-12-18 13:42:18 -0500

    user signup

--- src/routes/user.js
+++ src/routes/user.js
@@ -7,57 +7,57 @@ router.post('/signin', (req, res) => {
 
 router.post('/signup', (req, res) => {
     
-    User.findOne(
-        {
-          _id: mongojs.ObjectId(req.params.id)
-        },
-        (error, data) => {
-          if (error) {
-            res.send(error);
-          } else {
-            res.send(data);
-          }
-        }
-      );
+    User.findOne({ email: req.body.email })
+        .exec((error, user) => {
+            if (user) return res.status(400).json({
+                message: 'User already registered'
+            });
 
-    // User.findOne({ email: req.body.email })
-    //     .exec((error, user) => {
-    //         if (user) return res.status(400).json({
-    //             message: 'User already registered'
-    //         });
+            const { 
+                firstName, 
+                lastName, 
+                email, 
+                password 
+            } = req.body;
 
-    //         const { 
-    //             firstName, 
-    //             lastName, 
-    //             email, 
-    //             password 
-    //         } = req.body;
+            const _user = new User({
+                firstName, 
+                lastName, 
+                email, 
+                password, 
+                username: Math.random().toString()
+            });
 
-    //         const _user = new User({
-    //             firstName, 
-    //             lastName, 
-    //             email, 
-    //             password, 
-    //             username: Math.random().toString()
-    //         });
+            _user.save((error, data) => {
+                if (error) {
+                    return res.status(400).json({
+                        message: 'Something went awry'
+                    });
+                }
 
-    //         _user.save((error, data) => {
-    //             if (error) {
-    //                 return res.status(400).json({
-    //                     message: 'Something went awry'
-    //                 });
-    //             }
-
-    //             if (data) {
-    //                 return res.status(201).json({
-    //                     user: data
-    //                     // message: 'User created successfully'
-    //                 })
-    //             }
-    //         });
+                if (data) {
+                    return res.status(201).json({
+                        user: data
+                        // message: 'User created successfully'
+                    })
+                }
+            });
 
 
-    //     });
+        });
+
+      //   User.findOne(
+      //   {
+      //     _id: mongojs.ObjectId(req.params.id)
+      //   },
+      //   (error, data) => {
+      //     if (error) {
+      //       res.send(error);
+      //     } else {
+      //       res.send(data);
+      //     }
+      //   }
+      // );
 
 });
 
